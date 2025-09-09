918b008bf6054d07c60790ed59181a4c63f719bf
Paridhi Shah <84881091+paridhishah96@users.noreply.github.com>
2025-09-09 18:43:50 -0400

    i am tired of this

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    corePlugin = {
+      preflight: true,
+    }
+  ],
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ]
+  }
 })
